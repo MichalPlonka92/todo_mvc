eeb8d37d4a58c52f503c2ffc18ac6293e3d169a6
Michał Płonka <mplonka1992@gmail.com>
2023-05-29 15:14:03 +0200

    task change

--- todo-mvc/src/App.jsx
+++ todo-mvc/src/App.jsx
@@ -1,62 +1,73 @@
-import {useState} from "react";
+import { useState } from "react";
 
 function App() {
-  const [value, setValue] = useState( '');
-  const[tasks, setTasks] = useState([]);
+    const [value, setValue] = useState('');
+    const [tasks, setTasks] = useState([]);
 
-  function getID(){
-      if (!tasks.length) return 1;
+    function getID() {
+        if (!tasks.length) return 1;
 
-      return Math.max(...tasks.map((task) => task.id)) + 1
-  }
-  function handleInput (event){
-      setValue(event.target.value);
-  }
-  function handleAddTask(event){
-      if (event.key === 'Enter'){
-          setTasks([...tasks, {
-              id: getID(),
-              name: value,
-              status: 'active'
-           }]);
-          setValue('');
-      }
+        return Math.max(...tasks.map((task) => task.id)) + 1;
+    }
 
-  }
+    function handleInput(event) {
+        setValue(event.target.value);
+    }
 
-  function handleDelete(event) {
-      const id = parseInt(event.target.dataset.id);
-      const newTask = tasks.filter((task) => task.id !== id);
+    function handleAddTask(event) {
+        if (event.key === 'Enter') {
+            const newTask = {
+                id: getID(),
+                name: value,
+                status: 'active'
+            };
+            setTasks([...tasks, newTask]);
+            setValue('');
+        }
+    }
 
-      setTasks(newTask);
+    function handleDelete(event) {
+        const id = parseInt(event.target.dataset.id);
+        const newTasks = tasks.filter((task) => task.id !== id);
+        setTasks(newTasks);
+    }
 
-  }
+    function handleChangeStatus(task) {
+        return function () {
+            const updatedTasks = tasks.map((t) => {
+                if (t.id === task.id) {
+                    return { ...t, status: t.status === 'active' ? 'done' : 'active' };
+                }
+                return t;
+            });
+            setTasks(updatedTasks);
+        };
+    }
 
-
-
-  return (
-    <>
-      <h1>todos</h1>
-      <input
-          type="text"
-          value={value}
-          onChange={handleInput}
-          onKeyUp={handleAddTask}
-      />
-        <ul>
-            {tasks.map((task) => (
-                <li key={task.id}>
-                    <span>{task.name}</span>
-                    <button
-                    onClick={handleDelete}
-                    data-id={task.id}
-                    >delete
-                    </button>
-                </li>
-            ))}
-        </ul>
-    </>
-  )
+    return (
+        <>
+            <h1>todos</h1>
+            <input
+                type="text"
+                value={value}
+                onChange={handleInput}
+                onKeyUp={handleAddTask}
+            />
+            <ul>
+                {tasks.map((task) => (
+                    <li key={task.id}>
+                        <button onClick={handleChangeStatus(task)}
+                        className={task.status === 'active' ? 'done' : 'active'}
+                        >{task.status}</button>
+                        <span>{task.name}</span>
+                        <button onClick={handleDelete} data-id={task.id}>
+                            delete
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 }
 
-export default App
+export default App;
